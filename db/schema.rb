41c7965a0d73ce44f419db1010eaec8677545a58
Johan Esteves <jsesteves91@gmail.com>
2017-10-24 01:56:56 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20171024010118) do
 
-  create_table "artists", force: :cascade do |t|
-    t.string "name"
-  end
-
   create_table "songs", force: :cascade do |t|
     t.string "name"
   end
